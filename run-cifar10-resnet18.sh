6f89e2ba2664450afad4d8072dacf957bdb23863
yarkin06 <ayyildiz@ece.neu.edu>
2024-06-02 14:43:58 -0400

    loading pre-trained model debug

--- run-cifar10-resnet18.sh
+++ run-cifar10-resnet18.sh
@@ -1,38 +1,45 @@
 #!/bin/bash
 
-# dataset=cifar10
-# model=resnet18
-
-dataset=cifar100
-model=wrn28
-
-version=10
-cuda_device=1
+cuda_device=2
 
 yaml_version=2
+
+dataset=cifar10
+model=resnet18
+
+# dataset=cifar100
+# model=config/wrn28-${yaml_version}.yaml
+
+# dataset=cifar100
+# model=4
 
 
 # teacher=cifar10-resnet18-teacher.pt
 # teacher=cifar10-resnet18-kernel-npv2-pr0.pt
-teacher=cifar10-resnet18-kernel-npv${yaml_version}-vers${version}.pt
+teacher=cifar10-resnet18-kernel-npv${yaml_version}.pt
 
 
 # teacher=''
-# -lm ${teacher} 
+
 # LOAD MODEL YAPARKEN USTTEKINI KOYMAYI UNUTMA, -lm yi koy
 
 prune_finetune() {
     st=$5
-    save_name=${dataset}-${model}-$5-np$2-vers${version}-pr$4-lcm$3
+    save_name=${dataset}-${model}-$5-np$2-pr$4-lcm$3
     # save_name=${dataset}-${model}-$5-np$2-vers${version}-pr$4-lcm$3-lm${teacher}
     python -m source.core.run_partition -cfg config/${dataset}.yaml \
-        -mf ${save_name}.pt  \
-        --device $1 -np config/${model}-$2.yaml -st ${st} -pfl -lcm $3 -pr $4 -co \
-        -lr 0.01 -ep 3 -ree 2 -relr 0.001 \
+        -lm ${teacher} \
+        -mf ${save_name}.pt \
+        --device $1 \
+        -np config/${model}-$2.yaml \
+        -st ${st} \
+        -pfl -lcm $3 -pr $4 -co \
+        -lr 0.01 -ep 300 -ree 100 -relr 0.001 \
         >logs/${save_name}.out
 }
 
-prune_finetune "cuda:${cuda_device}" v${yaml_version} 0.001 0.2 kernel
+prune_finetune "cuda:${cuda_device}" v${yaml_version} 0.001 0.875 kernel
+
 
 #pretrainli yaparsan -lm i commentleyip, teacher='' olmasina gerek yok btw (cunku teacher i kullanan yok -lm haric), 
 #model adi = cifar10-resnet18-kernel-npv2-versx.pt, log u : cifar10-resnet18-kernel-npv2-versx-pr0.75-lcm0.001.out
