15c06ae456c1ca47a021fcc22066f5c340f6d45a
Braunn <augenbraun.n@northeastern.edu>
2024-09-08 16:51:29 -0400

    updated setup.sh

--- colosseum/setup.sh
+++ colosseum/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-## HOW TO RUN: ./setup.sh node_list.txt
+## HOW TO RUN: ./setup.sh node_list.txt [model name e.g. cifar10-resnet18-kernel-npv0-pr0.75-lcm0.001]
 ## node_list.txt should be formatted like:
-## type-srnnumber-mlnode,mlnode,mlnode
-## type-srnnumber-mlnode (etc)
+## type-srnnumber-network node
+## type-srnnumber-network node (etc)
+## TODO: include support for nodes using only part of the entire model 
+## Expects model name in argument to be on the file-proxy server here: /share/nas/genesys/CaP-Models
 ## if you are using cell, the base station is always the first node
 
 # Initialize a flag to track if cell or wifi nodes are encountered
@@ -19,7 +21,7 @@ for ((i=1; i<=num_lines; i++)); do
   # Split the line into three variables
   node_type=$(echo "$line" | cut -d'-' -f1)
   number=$(echo "$line" | cut -d'-' -f2)
-  nodes=$(echo "$line" | cut -d'-' -f3)
+  node=$(echo "$line" | cut -d'-' -f3)
 
   # Add the prefix 'genesys-' to the number portion
   prefixed_number="genesys-$number"
@@ -43,26 +45,17 @@ for ((i=1; i<=num_lines; i++)); do
       echo "Letting scope start"
       sleep 20  # Let the node start before moving on
 
-      # Sync local networks-for-ai directory with the remote
-      echo "Syncing local networks-for-ai directory with $prefixed_number"
-      sshpass -p "scope" rsync -avz --exclude='.git' --exclude='Archive/' --exclude='results/' ../../networks-for-ai/ $prefixed_number:/root/networks-for-ai/
+      # Sync local repo with the remote
+      echo "Syncing local repo with $prefixed_number"
+      sshpass -p "scope" rsync -avz --exclude='.git' ../../CaP/ $prefixed_number:/root/CaP/
       sleep 1
 
-      # Copy layer weights to each node in the list
-      IFS=',' read -ra node_array <<< "$nodes"
-      for node in "${node_array[@]}"; do
-        echo "Copying layer $node to $prefixed_number"
-        if [[ $node == 1 ]]; then
-          # Copy embedding weights.pth and tokenizer.model
-          sshpass -p "scope" ssh "$prefixed_number" "su srn-user -c 'cp /share/JARVIS/embedding_weights.pth /tmp/'"
-          sshpass -p "scope" ssh "$prefixed_number" "cp /tmp/embedding_weights.pth /root/networks-for-ai/weights/1.1-2b-it/" &
-          sshpass -p "scope" ssh "$prefixed_number" "su srn-user -c 'cp /share/JARVIS/tokenizer.model /tmp/'"
-          sshpass -p "scope" ssh "$prefixed_number" "cp /tmp/tokenizer.model /root/networks-for-ai/weights/1.1-2b-it/" &
-        fi
-        sshpass -p "scope" ssh "$prefixed_number" "su srn-user -c 'cp /share/JARVIS/*_$((node - 1)).pth /tmp/'"
-        sshpass -p "scope" ssh "$prefixed_number" "cp /tmp/*_$((node - 1)).pth /root/networks-for-ai/weights/1.1-2b-it/" &
-        sleep 2
-      done
+      # Copy model to network node      
+      echo "Copying $2.pt to $prefixed_number"
+      sshpass -p "scope" ssh "$prefixed_number" "su srn-user -c 'cp /share/CaP-Models/$2.pt /tmp/'"
+      sshpass -p "scope" ssh "$prefixed_number" "cp /tmp/$2.pt /root/CaP/assets/models/" &
+      sleep 2
+      
       ;;
 
     wifi)
@@ -76,57 +69,35 @@ for ((i=1; i<=num_lines; i++)); do
       fi
       sshpass -p "sunflower" ssh "$prefixed_number" "cd interactive_scripts && ./tap_setup.sh"
       sleep 5
-      #gnome-terminal -- bash -c "sshpass -p 'sunflower' ssh '$prefixed_number' 'cd interactive_scripts && ./modem_start.sh'; bash"
-      mintty -- bash -c "sshpass -p 'sunflower' ssh '$prefixed_number' 'cd interactive_scripts && ./modem_start.sh'; bash"
+      gnome-terminal -- bash -c "sshpass -p 'sunflower' ssh '$prefixed_number' 'cd interactive_scripts && ./modem_start.sh'; bash"
+      #mintty -- bash -c "sshpass -p 'sunflower' ssh '$prefixed_number' 'cd interactive_scripts && ./modem_start.sh'; bash"
       sleep 1
 
-      # Sync local networks-for-ai directory with the remote
-      echo "Syncing local networks-for-ai directory with $prefixed_number"
-      sshpass -p "sunflower" rsync -avz --exclude='.git' --exclude='Archive/' --exclude='results/' ../../networks-for-ai/ $prefixed_number:/root/networks-for-ai/
+      # Sync local repo with the remote
+      echo "Syncing local repo with $prefixed_number"
+      sshpass -p "sunflower" rsync -avz --exclude='.git'  ../../CaP/ $prefixed_number:/root/CaP/
       sleep 1
 
-      # Copy layer weights to each node in the list
-      IFS=',' read -ra node_array <<< "$nodes"
-      for node in "${node_array[@]}"; do
-        echo "Copying layer $node to $prefixed_number"
-        if [[ $node == 1 ]]; then
-          # Copy embedding weights.pth and tokenizer.model
-          sshpass -p "sunflower" ssh "$prefixed_number" "su srn-user -c 'cp /share/JARVIS/embedding_weights.pth /tmp/'"
-          sshpass -p "sunflower" ssh "$prefixed_number" "cp /tmp/embedding_weights.pth /root/networks-for-ai/weights/1.1-2b-it/" &
-          sshpass -p "sunflower" ssh "$prefixed_number" "su srn-user -c 'cp /share/JARVIS/tokenizer.model /tmp/'"
-          sshpass -p "sunflower" ssh "$prefixed_number" "cp /tmp/tokenizer.model /root/networks-for-ai/weights/1.1-2b-it/" &
-        fi
-        sshpass -p "sunflower" ssh "$prefixed_number" "su srn-user -c 'cp /share/JARVIS/*_$((node - 1)).pth /tmp/'"
-        sshpass -p "sunflower" ssh "$prefixed_number" "cp /tmp/*_$((node - 1)).pth /root/networks-for-ai/weights/1.1-2b-it/" &
-        sleep 2
-      done
-      ;;
+      # Copy model to network node      
+      echo "Copying $2.pt to $prefixed_number"
+      sshpass -p "sunflower" ssh "$prefixed_number" "su srn-user -c 'cp /share/CaP-Models/$2.pt /tmp/'"
+      sshpass -p "sunflower" ssh "$prefixed_number" "cp /tmp/$2.pt /root/CaP/assets/models/" &
+      sleep 2
 
     server)
       # Configuration for server type
       echo "Running configuration for server type..."
 
       # Sync local networks-for-ai directory with the remote
-      echo "Syncing local networks-for-ai directory with $prefixed_number"
-      sshpass -p "ChangeMe" rsync -avz --exclude='.git' --exclude='Archive/' --exclude='results/' ../../networks-for-ai/ $prefixed_number:/root/networks-for-ai/
+      echo "Syncing local repo with $prefixed_number"
+      sshpass -p "ChangeMe" rsync -avz --exclude='.git' ../../CaP/ $prefixed_number:/root/CaP/
       sleep 1
 
-      # Copy layer weights to each node in the list
-      IFS=',' read -ra node_array <<< "$nodes"
-      for node in "${node_array[@]}"; do
-        echo "Copying layer $node to $prefixed_number"
-        if [[ $node == 1 ]]; then
-          # Copy embedding weights.pth and tokenizer.model
-          sshpass -p "ChangeMe" ssh "$prefixed_number" "su srn-user -c 'cp /share/JARVIS/embedding_weights.pth /tmp/'"
-          sshpass -p "ChangeMe" ssh "$prefixed_number" "cp /tmp/embedding_weights.pth /root/networks-for-ai/weights/1.1-2b-it/" &
-          sshpass -p "ChangeMe" ssh "$prefixed_number" "su srn-user -c 'cp /share/JARVIS/tokenizer.model /tmp/'"
-          sshpass -p "ChangeMe" ssh "$prefixed_number" "cp /tmp/tokenizer.model /root/networks-for-ai/weights/1.1-2b-it/" &
-        fi
-        sshpass -p "ChangeMe" ssh "$prefixed_number" "su srn-user -c 'cp /share/JARVIS/*_$((node - 1)).pth /tmp/'"
-        sshpass -p "ChangeMe" ssh "$prefixed_number" "cp /tmp/*_$((node - 1)).pth /root/networks-for-ai/weights/1.1-2b-it/" &
-        sleep 2
-      done
-      ;;
+      # Copy model to network node      
+      echo "Copying $2.pt to $prefixed_number"
+      sshpass -p "ChangeMe" ssh "$prefixed_number" "su srn-user -c 'cp /share/CaP-Models/$2.pt /tmp/'"
+      sshpass -p "ChangeMe" ssh "$prefixed_number" "cp /tmp/$2.pt /root/CaP/assets/models/" &
+      sleep 2
 
     *)
       # Handle unknown types here
