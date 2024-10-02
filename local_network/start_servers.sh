852541fea2ae1c715cd3c52b8f765dc538090aa8
Braunn <augen@udel.edu>
2024-10-01 23:25:02 -0400

    started multiple runs comparison

--- local_network/start_servers.sh
+++ local_network/start_servers.sh
@@ -7,9 +7,9 @@
 ip_map_file="./config/resnet_4_network/ip-map.json"
 ip_map_file_win=".\config\resnet_4_network\ip-map.json"
 network_graph_file=".\config\resnet_4_network\network-graph.json"
-model_file="cifar10-resnet18-kernel-npv0-pr0.75-lcm0.001.pt" # this doesnt need full path, io utils handle it
+model_file="cifar10-resnet18-kernel-np4-pr0.85-lcm1e-05.pt" # this doesnt need full path, io utils handle it
 log_dir_name='test' # saves logging messages to ./logs/[log_dir_name] 
-debug='False' # checks each split model output and calculates FLOPS when true
+debug='True' # checks each split model output and calculates FLOPS when true
 
 for key in $(jq -r 'keys[]' $ip_map_file); do
 
