2425bf7b06cd818865fb5ca16389c57e080bb604
yarkin06 <ayyildiz@ece.neu.edu>
2024-10-15 13:46:20 -0400

    animation demo debug

--- sandbox/create_network_animation_main.py
+++ sandbox/create_network_animation_main.py
@@ -8,9 +8,10 @@ import matplotlib.lines as mlines
 import yaml
 
 path = os.path.join('logs', 'logs_colosseum')
-model_name = 'esc-EscFusion-kernel-np4-pr0.5-lcm100'
-# model_name = 'esc-EscFusion-kernel-np4-pr0.85-lcm100'
+# model_name = 'esc-EscFusion-kernel-np4-pr0.5-lcm100'
+model_name = 'esc-EscFusion-kernel-np4-pr0.85-lcm100'
 # model_name = 'cifar100-resnet101-kernel-np4-pr0.5-lcm1e-04-batch16'
+# model_name = 'cifar100-resnet101-kernel-np4-pr0.85-lcm1e-04-batch16'
 
 path_partition_file = os.path.join('config', 'EscFusion-np4.yaml')
 with open(path_partition_file, 'r') as stream:
@@ -22,15 +23,22 @@ with open(path_partition_file, 'r') as stream:
 partition_list = list(partition['partitions'].keys())
 partition_list = partition_list + ['out']
 
-scaling = 10
+slowing_factor = 2
+
+scaling = 1
+fps = 1000/(scaling * slowing_factor)
 node_size = 4000
 edge_width = 3
+arrow_size = 20
 
 model_path = os.path.join(path, model_name)
 df = pd.read_csv(os.path.join(model_path, 'block_events.csv'))
 
+# Max bytes sent
+max_bytes_sent = df['bytes_tx'].max()
+
 # Round up the max time to the nearest second
-max_time = round((df['time'].max())/scaling) + 100
+max_time = round((df['time'].max())/scaling) + int(fps)
 # df['timestamp'] = pd.to_datetime(df['timestamp'])
 
 # Create a NetworkX graph
@@ -54,7 +62,7 @@ edges = [(0,1), (0,2), (0,3), (1,2), (1,3), (2,3),
 G.add_edges_from(edges)
 
 # Initialize the figure
-fig, ax = plt.subplots(figsize=(10, 10), dpi=300)
+fig, ax = plt.subplots(figsize=(10, 8), dpi=300)
 ax.axis('off')  # Hide axes
 # nx.draw_networkx_nodes(G, positions, node_color='gray', node_size=node_size)
 # nx.draw_networkx_edges(G, positions, edge_color='gray', arrows=False, width=edge_width, node_size=node_size)
@@ -71,10 +79,10 @@ node_send = mlines.Line2D([], [], color='orange', marker='o', linestyle='None', 
 node_execute = mlines.Line2D([], [], color='blue', marker='o', linestyle='None', markersize=10, label='Execute (Node)')
 edge_idle = mlines.Line2D([], [], color='gray', linewidth=2, marker='', linestyle='-', markersize=10, label='Idle (Edge)')
 edge_send = mlines.Line2D([], [], color='orange', linewidth=2, marker='>', linestyle='-', markersize=10, label='Send (Edge)')
-edge_execute = mlines.Line2D([], [], color='blue', linewidth=2, marker='>', linestyle='-', markersize=10, label='Execute (Edge)')
+# edge_execute = mlines.Line2D([], [], color='blue', linewidth=2, marker='>', linestyle='-', markersize=10, label='Execute (Edge)')
 
 # Add legend to the plot
-network_ax.legend(handles=[node_idle, node_send, node_execute, edge_idle, edge_send, edge_execute], loc='upper right')
+network_ax.legend(handles=[node_idle, node_send, node_execute, edge_idle, edge_send], loc='upper right')
 
 progress_data = {node: 0 for node in positions.keys()}
 
@@ -92,6 +100,9 @@ def update(frame):
     activated_edges = []
     activated_edges_color = []
 
+    edge_width_active = edge_width
+    arrow_size_active = arrow_size
+
     # Iterate through the dataframe to check for active processes
     for i, row in df.iterrows():
         start_time = row['time']
@@ -102,6 +113,9 @@ def update(frame):
         if start_time <= frame * scaling < start_time + duration:  # frame * 1000 to convert from frames to ms
             current_type = row['type']
             if current_type == 'send':
+                # Arrange edge_width based on the number of bytes sent
+                edge_width_active = 3 + 5 * (row['bytes_tx'] / max_bytes_sent)
+                arrow_size_active = 20 + 35 * (row['bytes_tx'] / max_bytes_sent)
                 target_node = int(row['port'] - 5000)  # Map port to node index
                 node_colors[current_node] = 'orange'
                 node_colors[target_node] = 'orange'
@@ -124,10 +138,10 @@ def update(frame):
 
     nx.draw_networkx_nodes(G, positions, ax=network_ax, node_color=node_colors, node_size=node_size)
     nx.draw_networkx_edges(G, positions, ax=network_ax, edgelist=[edge for edge in edges if edge not in activated_edges], edge_color='gray', arrows=False, width=edge_width, node_size=node_size)
-    nx.draw_networkx_edges(G, positions, ax=network_ax, edgelist=activated_edges, edge_color=activated_edges_color, arrowstyle='-|>', arrowsize=20, width=edge_width, node_size=node_size)
+    nx.draw_networkx_edges(G, positions, ax=network_ax, edgelist=activated_edges, edge_color=activated_edges_color, arrowstyle='-|>', arrowsize=arrow_size_active, width=edge_width_active, node_size=node_size*0.75)
     nx.draw_networkx_labels(G, positions, ax=network_ax, labels=labels, font_color='black')
 
-    network_ax.legend(handles=[node_idle, node_send, node_execute, edge_idle, edge_send, edge_execute], loc='upper right')
+    network_ax.legend(handles=[node_idle, node_send, node_execute, edge_idle, edge_send], loc='upper right')
 
     # Draw progress bars for each node
     # for node in positions.keys():
@@ -171,6 +185,6 @@ ani = animation.FuncAnimation(fig, update, frames=max_time, interval=1000, repea
 # Save the animation
 plt.axis('off')
 
-ani.save(f'{model_name}_main.mp4', writer='ffmpeg', fps=100, dpi=300)
+ani.save(f'{model_name}_main.mp4', writer='ffmpeg', fps=fps, dpi=300)
 
 plt.close(fig)
